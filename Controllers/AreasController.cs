bd5c769ad2130d57b5345d246f30bc3bed0f0be7
mondayscool <76455195+mondayscool@users.noreply.github.com>
2023-05-17 17:06:21 -0700

    patch support for areas

--- Controllers/AreasController.cs
+++ Controllers/AreasController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Options;
 using PitchLogAPI.Helpers;
 using PitchLogAPI.Model;
 using PitchLogAPI.ResourceParameters;
@@ -13,13 +15,13 @@ namespace PitchLogAPI.Controllers
     [ApiController]
     public class AreaController : ControllerBase
     {
-        private readonly IAreasRepository _repository;
+        private readonly IAreasRepository _areasRepository;
         private readonly IMapper _mapper;
         private readonly ProblemDetailsFactory _problemDetailsFactory;
 
-        public AreaController(IAreasRepository repository, IMapper mapper, ProblemDetailsFactory problemDetailsFactory)
+        public AreaController(IAreasRepository areasRepository, IMapper mapper, ProblemDetailsFactory problemDetailsFactory)
         {
-            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _areasRepository = areasRepository ?? throw new ArgumentNullException(nameof(areasRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _problemDetailsFactory = problemDetailsFactory ?? throw new ArgumentNullException(nameof(problemDetailsFactory));
         }
@@ -27,7 +29,7 @@ namespace PitchLogAPI.Controllers
         [HttpGet(Name = nameof(GetAreas))]
         public async Task<IActionResult> GetAreas([FromQuery] AreasResourceParameters parameters)
         {
-            var areas = await _repository.GetCollection(parameters);
+            var areas = await _areasRepository.GetCollection(parameters);
             Response.AddPaginationHeaders(areas, Request.GetAbsoluteUri());
 
             var areasToReturn = _mapper.Map<IEnumerable<AreaDTO>>(areas);
@@ -46,7 +48,7 @@ namespace PitchLogAPI.Controllers
         [HttpGet("{ID}", Name = nameof(GetAreaByID))]
         public async Task<IActionResult> GetAreaByID(int ID)
         {
-            var area = await _repository.GetByID(ID);
+            var area = await _areasRepository.GetByID(ID);
 
             if(area == null)
             {
@@ -62,7 +64,7 @@ namespace PitchLogAPI.Controllers
         [HttpPost(Name = nameof(CreateArea))]
         public async Task<IActionResult> CreateArea(AreaForCreationDTO areaForCreation)
         {
-            if(await _repository.Exists(areaForCreation.Name))
+            if(await _areasRepository.Exists(areaForCreation.Name))
             {
                 return BadRequest(_problemDetailsFactory.CreateProblemDetails(
                     HttpContext, statusCode: 400, detail: $"Area with the name {areaForCreation.Name} already exists."));
@@ -70,8 +72,8 @@ namespace PitchLogAPI.Controllers
 
             var areaToCreate = _mapper.Map<Area>(areaForCreation);
            
-            _repository.Create(areaToCreate);
-            await _repository.Save();
+            _areasRepository.Create(areaToCreate);
+            await _areasRepository.Save();
 
             var areaToReturn = _mapper.Map<AreaDTO>(areaToCreate);
             AddLinksToArea(areaToReturn);
@@ -82,45 +84,57 @@ namespace PitchLogAPI.Controllers
         [HttpPut("{ID}", Name = nameof(UpdateAreaFull))]
         public async Task<IActionResult> UpdateAreaFull(int ID, AreaForUpdateDTO areaForUpdate)
         {
-            var area = await _repository.GetByID(ID);
+            var area = await _areasRepository.GetByID(ID);
 
             if(area == null)
             {
-                area = _mapper.Map<Area>(areaForUpdate);
-                area.ID = ID;
-
-                _repository.Create(area);
-                await _repository.Save();
-
-                var areaToReturn = _mapper.Map<AreaDTO>(area);
-
-                return CreatedAtRoute(nameof(GetAreaByID), new { ID }, areaToReturn);
+                return NotFound();
             }
 
             _mapper.Map(areaForUpdate, area);
-            await _repository.Save();
+            await _areasRepository.Save();
 
             return NoContent();
         }
 
         [HttpPatch("{ID}", Name = nameof(UpdateAreaPartial))]
-        public async Task<IActionResult> UpdateAreaPartial(AreaForUpdateDTO areaForUpdate)
+        public async Task<IActionResult> UpdateAreaPartial(int ID, JsonPatchDocument<AreaForUpdateDTO> patchDoc)
         {
-            throw new NotImplementedException();
+            var area = await _areasRepository.GetByID(ID);
+
+            if(area == null)
+            {
+                return NotFound();
+            }
+
+            var areaToPatch = _mapper.Map<AreaForUpdateDTO>(area);
+            patchDoc.ApplyTo(areaToPatch);
+
+            if(!TryValidateModel(areaToPatch))
+            {
+                return (IActionResult)HttpContext.RequestServices.GetRequiredService<IOptions<ApiBehaviorOptions>>()
+                    .Value.InvalidModelStateResponseFactory(ControllerContext);
+            }
+
+            _mapper.Map(areaToPatch, area);
+
+            await _areasRepository.Save();
+
+            return NoContent();
         }
 
         [HttpDelete("{ID}", Name = nameof(DeleteArea))]
         public async Task<IActionResult> DeleteArea(int ID)
         {
-            var areaToDelete = await _repository.GetByID(ID);
+            var areaToDelete = await _areasRepository.GetByID(ID);
 
             if(areaToDelete == null)
             {
                 return NotFound();
             }
 
-            _repository.Delete(areaToDelete);
-            _repository.Save();
+            _areasRepository.Delete(areaToDelete);
+            _areasRepository.Save();
 
             return NoContent();
         }
